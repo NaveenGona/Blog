13f2c51a243cb14a3d2ab23a475335807759cfa6
naveen gona <naveen.gona@outlook.com>
2021-06-04 17:33:29 +0530

    New page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,27 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainBodyComponent } from './main-body/main-body.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainBodyComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes),
+    AppRoutingModule,
+
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [
